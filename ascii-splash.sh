6ea3d79b1030dd7051038ec57310c626421fd9ce
Code Slicer <ivanaugustobd@pm.me>
2022-12-26 20:18:59 -0300

    review: greatly improved splash screen

--- ascii-splash.sh
+++ ascii-splash.sh
@@ -1,27 +1,31 @@
 #!/bin/bash -e
 
-DG_LOGO='#f16323'
-DI_LOGO='#7389dc'
-IN_LOGO='#c13584'
-SP_LOGO='#1ed760'
-TW_LOGO='#1d9bf0'
-YT_LOGO='#f00'
+DG='#f16323'
+DI='#7389dc'
+IN='#c13584'
+SP='#1ed760'
+TW='#1d9bf0'
+YT='#f00'
 
 echo ''
-echo "           {{ Foreground \"$DG_LOGO\" \"0KkxooooxkK0\" }}"
-echo "       {{ Foreground \"$DG_LOGO\" \"dKkl;.\" }}         {{ Foreground \"$DG_LOGO\" \".lkKd\" }}        "
-echo "     {{ Foreground \"$DG_LOGO\" \"Xk:.\" }}                  {{ Foreground \"$DG_LOGO\" \":X\" }}      "
-echo "   {{ Foreground \"$DG_LOGO\" \"ok:.\" }}              MMMM0  {{ Foreground \"$DG_LOGO\" \":ko\" }}    {{ Bold \"DISCORGENTO\" }} · {{ Underline \"https://discorgento.com/\" }}"
-echo "  {{ Foreground \"$DG_LOGO\" \"do.\" }}                MMMM0   {{ Foreground \"$DG_LOGO\" \".od\" }}   {{ Italic \"Magento 2 development made easy\" }}"
-echo " {{ Foreground \"$DG_LOGO\" \":o.\" }}        ,oOKXXKOkMMMM0    {{ Foreground \"$DG_LOGO\" \".o:\" }}  "
-echo " {{ Foreground \"$DG_LOGO\" \"d;\" }}       lNMMMMMMMMMMMMM0     {{ Foreground \"$DG_LOGO\" \";d\" }}  "
-echo " {{ Foreground \"$DG_LOGO\" \"o.\" }}      KMMMMNxllxNMMMMM0     {{ Foreground \"$DG_LOGO\" \".o\" }}  {{ Foreground \"$DI_LOGO\" \"Discord\" }}: {{ Underline \"https://discord.io/Discorgento\" }}"
-echo " {{ Foreground \"$DG_LOGO\" \"c.\" }}     dMMMMx      xMMMM0     {{ Foreground \"$DG_LOGO\" \".c\" }}  GitHub: {{ Underline \"https://github.com/discorgento\" }}"
-echo " {{ Foreground \"$DG_LOGO\" \"\\\":\" }}     kMMMM:      :MMMMO     {{ Foreground \"$DG_LOGO\" \";,\" }}  {{ Foreground \"$IN_LOGO\" \"Instagram\" }}: {{ Underline \"https://instagram.com/discorgento\" }}"
-echo "  {{ Foreground \"$DG_LOGO\" \";.\" }}    \"MMMMWl.  .lWMMMM,    {{ Foreground \"$DG_LOGO\" \".;\" }}   {{ Foreground \"$SP_LOGO\" \"Spotify\" }}: {{ Underline \"https://sptlnk.com/discorgento\" }}"
-echo "   {{ Foreground \"$DG_LOGO\" \"\\\".\" }}     NMMMMMMMMMMMMN,    {{ Foreground \"$DG_LOGO\" \".,\" }}    {{ Foreground \"$TW_LOGO\" \"Twitter\" }}: {{ Underline \"https://twitter.com/discorgento\" }}"
-echo "     {{ Foreground \"$DG_LOGO\" \";.\" }}    :OWMMMMMMWO,     {{ Foreground \"$DG_LOGO\" \";\" }}      {{ Foreground \"$YT_LOGO\" \"YouTube\" }}: {{ Underline \"https://youtube.com/@discorgento\" }}"
-echo "       {{ Foreground \"$DG_LOGO\" \"\\\"..\" }}   '\"\"\"\"\"\"\"'    {{ Foreground \"$DG_LOGO\" \".'\" }}        "
-echo "          {{ Foreground \"$DG_LOGO\" \"''..\" }}        {{ Foreground \"$DG_LOGO\" \"..''\" }}            "
-echo "              {{ Foreground \"$DG_LOGO\" \"''''''''\" }}                 "
+echo "            {{ Foreground \"$DG\" \".:-==========-:.\" }}              {{ Foreground \"$DG\" (Bold \"DISCORGENTO\") }} · {{ Underline \"https://discorgento.com\" }}"
+echo "        {{ Foreground \"$DG\" \".:==-:..\" }}         {{ Foreground \"$DG\" \".:-=-:.\" }}          {{ Italic \"M2 development made\" }} {{ CrossOut (Italic \"easy\") }} {{ Italic \"less painful\" }}"
+echo "      {{ Foreground \"$DG\" \":==:.\" }}                 {{ Foreground \"$DG\" \".:-==:\" }}        "
+echo "    {{ Foreground \"$DG\" \".==.\" }}                -@@@@@% {{ Foreground \"$DG\" \".==.\" }}      {{ Foreground \"$DI\" \"Discord\" }}"
+echo "   {{ Foreground \"$DG\" \"-+:\" }}                  -@@@@@@   {{ Foreground \"$DG\" \":+-\" }}     {{ Underline \"https://discord.io/Discorgento\" }}"
+echo "  {{ Foreground \"$DG\" \"==\" }}                    -@@@@@%     {{ Foreground \"$DG\" \"==\" }}    "
+echo " {{ Foreground \"$DG\" \"-+\" }}           .-+*#%%#*+*@@@@@%      {{ Foreground \"$DG\" \"+-\" }}   GitHub:"
+echo "{{ Foreground \"$DG\" \".+:\" }}         -#@@@@@@@@@@@@@@@@%      {{ Foreground \"$DG\" \":+.\" }}  {{ Underline \"https://github.com/discorgento\" }}"
+echo "{{ Foreground \"$DG\" \"==\" }}        .#@@@@@@@@@@@@@@@@@@%       {{ Foreground \"$DG\" \"==\" }}  "
+echo "{{ Foreground \"$DG\" \"+-\" }}       .@@@@@@@*=--=*@@@@@@@%       {{ Foreground \"$DG\" \"-+\" }}  {{ Foreground \"$IN\" \"Instagram\" }}"
+echo "{{ Foreground \"$DG\" \"+-\" }}       *@@@@@%.      .%@@@@@%       {{ Foreground \"$DG\" \"-+\" }}  {{ Underline \"https://instagram.com/discorgento\" }}"
+echo "{{ Foreground \"$DG\" \"==\" }}       %@@@@@-        -@@@@@%       {{ Foreground \"$DG\" \"==\" }}  "
+echo "{{ Foreground \"$DG\" \".+:\" }}      #@@@@@#        #@@@@@#      {{ Foreground \"$DG\" \":+.\" }}  {{ Foreground \"$SP\" \"Spotify\" }}"
+echo " {{ Foreground \"$DG\" \"-+\" }}      :@@@@@@%=:..:=%@@@@@@:      {{ Foreground \"$DG\" \"+-\" }}   {{ Underline \"https://sptlnk.com/discorgento\" }}"
+echo "  {{ Foreground \"$DG\" \"==\" }}      -@@@@@@@@@@@@@@@@@@-      {{ Foreground \"$DG\" \"==\" }}    "
+echo "   {{ Foreground \"$DG\" \"-+:\" }}     .+@@@@@@@@@@@@@@+.     {{ Foreground \"$DG\" \":+-\" }}     {{ Foreground \"$TW\" \"Twitter\" }}"
+echo "    {{ Foreground \"$DG\" \".==.\" }}      -*#%@@@@%#*-      {{ Foreground \"$DG\" \".==.\" }}      {{ Underline \"https://twitter.com/discorgento\" }}"
+echo "      {{ Foreground \"$DG\" \":==:.\" }}                  {{ Foreground \"$DG\" \".:==:\" }}        "
+echo "        {{ Foreground \"$DG\" \".:==-:..\" }}        {{ Foreground \"$DG\" \"..:-==:.\" }}          {{ Foreground \"$YT\" \"YouTube\" }}"
+echo "            {{ Foreground \"$DG\" \".:-==========-:.\" }}              {{ Underline \"https://youtube.com/@discorgento\" }}"
 echo ''
